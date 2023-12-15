51aac275dd3af1501b5135116bc49b2416c75f32
Home K&M <ecaterina.zah@gmail.com>
2023-12-15 17:38:41 +0200

    New Project Books

---
+++ classic_books/src/main/java/com/ecaterina_project_book/classic_books/ClassicBooksApplication.java
@@ -0,0 +1,13 @@
+package com.ecaterina_project_book.classic_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicBooksApplication.class, args);
+	}
+
+}
---
+++ classic_books/src/test/java/com/ecaterina_project_book/classic_books/ClassicBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecaterina_project_book.classic_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
